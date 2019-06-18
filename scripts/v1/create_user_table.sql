9feee4c6b641b540deea7aa057b63c7a92fe6437
ShobanaBohs100 <letter2shobana@gmail.com>
2019-06-18 17:11:57 +0530

    v1 scripts updated

---
+++ scripts/v1/create_user_table.sql
@@ -0,0 +1,11 @@
+USE sql7295922;
+CREATE TABLE Persons
+(
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+        
